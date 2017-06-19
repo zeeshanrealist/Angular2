c03220a5410f1e0b8a121ecf7d177361803d8da9
gowthamanm <gowthamanm@ideas2it.com>
2017-06-19 14:45:44 +0530

    Complete Angular2 Tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+import { HeroesComponent } from './heroes.component';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>{{title}}</h1>
+             <nav>
+               <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
+               <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
+             </nav>
+             <router-outlet></router-outlet>
+             `,
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  { name = 'Angular'; }
+
+export class AppComponent  { 
+  title = 'Tour of Heroes';
+}
+
+
+
+
